a6c51ec656d3024a055d0d52320fad7889c6c990
Gifted <epopoola15@gmail.com>
2024-12-30 09:15:34 +0100

    Add binary trees data structure

--- data_structures/7_Binary_trees/bin.py
+++ data_structures/7_Binary_trees/bin.py
@@ -106,6 +106,26 @@ class BinarySearchTree:
             return self.right.find_max()
         return self.data
     
+    def delete(self, val):
+        if val < self.data: # if value is less than the root data
+            if self.left:  # Then it will be in the left node, if the left node exists
+                self.left = self.left.delete(val) # then recursively check through the left node 
+        elif val > self.data: # if the value is now greater than the root data
+            if self.right: #then the value is at the right node, check if right node exist
+                self.right = self.right.delete(val) # then recursively check through the right node for the value
+        else: # When the value equates the data, i.e. when val == self.data
+            if self.left is None and self.right is None: # if there are no leaves node
+                return None # delete the node i.e. self.data
+            if self.left is None: # if there is only one child at the right child and none at the left
+                return self.right # return the right child 
+            if self.right is None: # if there is only one child at the left node and none at the right
+                return self.left # return the left child
+            
+            max = self.left.find_max() # find the largest value at the left sub tree
+            self.data = max # equate it to the root data
+            self.left = self.left.delete(max) # delete the copy of the max node
+        return self
+
 
     def sum_elements(self):
         # Although this works
@@ -141,3 +161,5 @@ if __name__ == '__main__':
     print(root_tree.find_min())
     print(root_tree.find_max())
     print(root_tree.sum_elements())
+    root_tree.delete(16git)
+    print(root_tree.inorder_traversal())
